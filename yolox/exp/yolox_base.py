9dce52c9619fcc5311a3cd403f13f03b26017ae9
chenjl <chenjl@elimen.com.cn>
2021-10-13 19:31:09 +0800

    code review

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -17,7 +17,7 @@ class Exp(BaseExp):
         super().__init__()
 
         # ---------------- model config ---------------- #
-        self.num_classes = 2 #80
+        self.num_classes = 80 #80
         self.depth = 1.00
         self.width = 1.00
 
@@ -81,7 +81,7 @@ class Exp(BaseExp):
     def get_data_loader(self, batch_size, is_distributed, no_aug=False):
         from yolox.data import (
             COCODataset,
-            TablebankDataset,
+            #TablebankDataset,
             DataLoader,
             InfiniteSampler,
             MosaicDetection,
@@ -89,15 +89,15 @@ class Exp(BaseExp):
             YoloBatchSampler
         )
 
-        dataset = TablebankDataset(
-            imgs_path=None,
-            anno_path=self.train_ann,
-            resize_size=self.input_size,
-            is_train=True,
-            transform=None
-        )
+        # _dataset_ = TablebankDataset(
+        #     imgs_path=None,
+        #     anno_path=self.train_ann,
+        #     resize_size=self.input_size,
+        #     is_train=True,
+        #     transform=None
+        # )
 
-        _dataset_ = COCODataset(
+        dataset = COCODataset(
             data_dir=None,
             json_file=self.train_ann,
             img_size=self.input_size,
